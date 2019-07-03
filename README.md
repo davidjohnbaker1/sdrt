aa56628df6a13a2047b6165f0f7b430e9852ca76
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2019-07-03 12:51:50 +0100

    skeleton there for nine recall :)

--- README.md
+++ README.md
@@ -11,9 +11,8 @@ Experiment lives [here](https://musiccog.lsu.edu/sdrt/sdrt.html)
 - Export all quintile lists to data for sdrt 
 - Turn all stimuli to mp3 
 - Load in as appropriate waves in experiment 
+- check download sheet that can score 
 
-- Add in multi stim collection
-- change to scale degrees
 - check return 
 - add in plus watch with timer
 - write minidyom 
---
+++ demographic_questions.js
@@ -0,0 +1,34 @@
+/* Demographic Information */
+
+var survey_trial_1 = {
+  type: 'survey-text',
+  questions: [{prompt: "How many years old are you?"}]
+};
+
+
+var survey_trial_2 = {
+  type: 'survey-text',
+  questions: [{prompt: "What is your educational status? (e.g. No Higher Education, Bachelor's, Masters Student, Finished Terminal Degree)"}]
+};
+
+var survey_trial_3 = {
+  type: 'survey-text',
+  questions: [{prompt: "Which type of syllable system do you prefer to use? (e.g. Moveable-Do, Fixed-Do, La-Based Minor, Numbers"}]
+};
+
+var survey_trial_4 = {
+  type: 'survey-text',
+  questions: [{prompt:"Do you have absolute pitch?"}]
+};
+
+var survey_trial_5 = {
+  type: 'survey-text',
+  questions: [{prompt: "How many weeks of aural skills training have you completed? (1 Semester = 15 weeks, 4 semesters = 60 weeks)"}]
+};
+
+var survey_trial_6 = {
+  type: 'survey-text',
+  questions: [{prompt: "How many years have you taught aural skills at the post-secondary level? (If you are an undergraduate student, please respond with 0)"}]
+};
+
+
